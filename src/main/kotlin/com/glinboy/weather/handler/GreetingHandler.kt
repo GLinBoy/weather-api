ade2f987c72da09a7bca2381463d314e82c9d27e
GLinBoy <hojjatabedie@gmail.com>
2022-03-15 20:58:57 +0330

    📦 NEW: Added a simple handler

---
+++ src/main/kotlin/com/glinboy/weather/handler/GreetingHandler.kt
@@ -0,0 +1,15 @@
+package com.glinboy.weather.handler
+
+import org.springframework.http.MediaType
+import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.BodyInserters
+import org.springframework.web.reactive.function.server.ServerRequest
+import org.springframework.web.reactive.function.server.ServerResponse
+import reactor.core.publisher.Mono
+
+@Component
+class GreetingHandler {
+    fun hello(request: ServerRequest): Mono<ServerResponse> =
+        ServerResponse.ok().contentType(MediaType.APPLICATION_JSON)
+            .body(BodyInserters.fromValue("Hello Spring Reactive!!!"))
+}
