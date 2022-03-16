526e8c7be7309862aff15cd0650d36ea2a9008c0
GLinBoy <hojjatabedie@gmail.com>
2022-03-16 21:36:26 +0330

    📦 NEW: Added OpenWeatherMapService interface

---
+++ src/main/kotlin/com/glinboy/weather/service/OpenWeatherMapService.kt
@@ -0,0 +1,8 @@
+package com.glinboy.weather.service
+
+import com.glinboy.weather.dto.*
+import org.springframework.util.MultiValueMap
+import reactor.core.publisher.Mono
+
+interface OpenWeatherMapService {
+}
