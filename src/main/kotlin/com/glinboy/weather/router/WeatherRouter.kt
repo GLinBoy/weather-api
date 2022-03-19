f7d279f931cb532a51c2fdb541cda4b417f0a1ea
GLinBoy <hojjatabedie@gmail.com>
2022-03-19 11:33:16 +0330

    📦 NEW: Added WeatherRouter

---
+++ src/main/kotlin/com/glinboy/weather/router/WeatherRouter.kt
@@ -0,0 +1,13 @@
+package com.glinboy.weather.router
+
+import com.glinboy.weather.handler.WeatherHandler
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.MediaType
+import org.springframework.web.reactive.function.server.RequestPredicates
+import org.springframework.web.reactive.function.server.RouterFunction
+import org.springframework.web.reactive.function.server.RouterFunctions
+import org.springframework.web.reactive.function.server.ServerResponse
+
+@Configuration(proxyBeanMethods = false)
+class WeatherRouter {}
