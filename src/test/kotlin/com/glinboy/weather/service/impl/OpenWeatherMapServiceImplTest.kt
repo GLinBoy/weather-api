0c94b76bc0fef0660ba09df7239321ebabef20e3
GLinBoy <hojjatabedie@gmail.com>
2022-03-28 10:47:28 +0430

    📦 NEW: Added test file for OpenWeatherMapServiceImpl

---
+++ src/test/kotlin/com/glinboy/weather/service/impl/OpenWeatherMapServiceImplTest.kt
@@ -0,0 +1,32 @@
+package com.glinboy.weather.service.impl
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class OpenWeatherMapServiceImplTest {
+
+    @Test
+    fun climaticForecast30Days() {
+    }
+
+    @Test
+    fun historicalWeather() {
+    }
+
+    @Test
+    fun forecast5Day3Hour() {
+    }
+
+    @Test
+    fun searchWeather() {
+    }
+
+    @Test
+    fun dailyForecast16Days() {
+    }
+
+    @Test
+    fun currentWeather() {
+    }
+}
